bbcffbcdb4ebdd1488650ea72793af68199f4380
Weichen Liu <WeichenLiu@users.noreply.github.com>
2018-03-08 16:48:18 -0800

    Velocity

--- Assets/Script/MotionController.cs
+++ Assets/Script/MotionController.cs
@@ -12,7 +12,8 @@ public class MotionController : MonoBehaviour {
 	public Quaternion angleVelocity;
 
 	public Vector3 pivotCoord;
-	public Quaternion pivotRot;
+	public Vector3 pivotDir;
+	public Vector3 pivotRot;
 
 	public float vFalloff = 0.99f;
 	public float wFalloff = 0.85f;
@@ -28,32 +29,54 @@ public class MotionController : MonoBehaviour {
 	void Start () {
 		velocity = new Vector3 ();
 		angleVelocity = Quaternion.identity;
+		pivotRot = rightHand.transform.localPosition.normalized;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		
 		OVRInput.Update ();
-		if (OVRInput.Get (OVRInput.Axis1D.PrimaryIndexTrigger, OVRInput.Controller.RTrackedRemote)>0.0001f) {
+		if (OVRInput.Get (OVRInput.Axis1D.PrimaryIndexTrigger, OVRInput.Controller.RTouch)>0.0001f) {
+			Debug.Log ("triggered");
 			if (!triggered) {
 				pivotCoord = rightHand.transform.position;
-				pivotRot = rightHand.transform.rotation;
+				//pivotRot = rightHand.transform.localPosition;
 				triggered = true;
 			} else {
 				velocity = pivotCoord - rightHand.transform.position;
-				angleVelocity = Quaternion.Inverse(pivotRot) * rightHand.transform.rotation;
-				player.transform.position += velocity;
+				angleVelocity = Quaternion.Inverse(Quaternion.Euler(pivotRot)) * Quaternion.Euler(rightHand.transform.localPosition.normalized);
+				//angleVelocity = Quaternion.Slerp (Quaternion.identity, angleVelocity, 0.2f);
+
+				//angleVelocity *= angleVelocity;
+				Debug.Log(angleVelocity.eulerAngles);
+				//player.transform.position += velocity;
+				//player.transform.rotation *= angleVelocity;
+
+				Vector3 dir = player.transform.position - pivotCoord; // get point direction relative to pivot
+				dir = angleVelocity * dir; // rotate it
 				player.transform.rotation *= angleVelocity;
+				//player.transform.position = dir + pivotCoord; // calculate rotated point
+
 			}
 		} else {
 			triggered = false;
-			if (velocity.magnitude >= 0.01f) {
-				player.transform.position += velocity;
+			if (velocity.magnitude >= 0.00001f) {
+				//player.transform.position += velocity;
 				velocity *= vFalloff;
 			}
 			if(Quaternion.Angle(angleVelocity, Quaternion.identity) >= 0.005f){
+				//player.transform.rotation *= angleVelocity;
+
+				Vector3 dir = player.transform.position - pivotCoord; // get point direction relative to pivot
+				dir = angleVelocity * dir; // rotate it
+				//angleVelocity = Quaternion.Slerp (Quaternion.identity, angleVelocity, 0.1f);
 				player.transform.rotation *= angleVelocity;
+				//player.transform.position = dir + pivotCoord; // calculate rotated point
+
 				angleVelocity = Quaternion.Slerp (Quaternion.identity, angleVelocity, wFalloff);
 			}
 		}
+
+		pivotRot = rightHand.transform.localPosition.normalized;
 	}
 }
