74f35e7c19fe3e262b24fa7a04e0eacea11f64de
Weichen Liu <WeichenLiu@users.noreply.github.com>
2018-03-17 19:36:52 -0700

    Grabbable

---
+++ Assets/Scripts/HandTriggerBox.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandTriggerBox : MonoBehaviour
+{
+
+    public GameObject parent;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    this.transform.position = parent.transform.position;
+	    this.transform.rotation = parent.transform.rotation;
+	}
+}
