9387fef248f341eb25ee1a1926df4110b179b5ef
Weichen Liu <WeichenLiu@users.noreply.github.com>
2018-03-18 03:49:09 -0700

    Add some camera effects

---
+++ Assets/Standard Assets/Effects/Darken/DarkenEffect.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.ImageEffects;
+
+[RequireComponent(typeof(Camera))]
+public class DarkenEffect : PostEffectsBase
+{
+    Material m;
+    public Shader shader;
+    Material gm;
+    public Shader gshader;
+
+    public float ratio = 1.0f;
+
+    public bool greyscale = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        m = new Material(shader);
+        gm = new Material(gshader);
+    }
+
+    // Update is called once per frame
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+
+        if (greyscale)
+        {
+            gm.SetFloat("_ratio", ratio);
+            Graphics.Blit(source, destination, gm);
+        }
+        else
+        {
+            m.SetFloat("_ratio", ratio);
+            Graphics.Blit(source, destination, m);
+        }
+    }
+}
