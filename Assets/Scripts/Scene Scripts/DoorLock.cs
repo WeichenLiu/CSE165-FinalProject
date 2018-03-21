251c7320a28d2601d7dd2ba5a00b9da4dbeb022d
Ziyang Li <liby99@icloud.com>
2018-03-21 14:17:49 -0700

    Add Door Lock Script

---
+++ Assets/Scripts/Scene Scripts/DoorLock.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorLock : MonoBehaviour {
+
+    public GameObject Card;
+    public float ActivateDistance;
+    public GameController Controller;
+    public float ActivateTime;
+
+    private bool activated;
+    private bool hovering;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    float GetDistance()
+    {
+        return (transform.position - Card.transform.position).magnitude;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (!hovering && GetDistance() < ActivateDistance)
+        {
+            hovering = true;
+            if (!activated)
+            {
+                
+            }
+        }
+        else if (hovering && GetDistance() >= ActivateDistance)
+        {
+            hovering = false;
+        }
+	}
+
+    public void SwitchOn()
+    {
+        
+    }
+
+    public void Activate()
+    {
+        activated = true;
+        // Controller.openDoor();
+        StartCoroutine("ActivateUI", Time.time);
+    }
+
+    IEnumerator ActivateUI(float startTime)
+    {
+        while (Time.time - startTime < ActivateTime)
+        {
+            yield return null;
+        }
+        SwitchOn();
+    }
+}
